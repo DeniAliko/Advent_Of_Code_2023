0cd4a442fae5ee5cb6cd35a512b806f0231d2e97
DeniAliko <denialiko2016@gmail.com>
2023-12-06 07:51:15 -0500

    attempting recursive solution for 5

--- day5.py
+++ day5.py
@@ -89,75 +89,50 @@ for seed in starts:
 
 print(locations)
 print("min", min(locations))
+types = ["soil", "fert", "water", "light", "temp", "humid", "loc"]
 
-minLoc = 0
+nStarts = [[starts[i], starts[i] + starts[i+1]] for i in range(0, len(starts), 2)]
 
-for i in range(0, len(starts), 2):
-    for seed in range(starts[i], starts[i] + starts[i+1]):
-        for map in maps[0]:
-            if seed >= map[1] and seed <= map[1] + map[2] - 1:
-                soil = map[0] + (seed - map[1])
-                found = True
-        if not found:
-            soil = seed
-        # print(soil)
-        found = False
+locationRanges = []
+def mapTo(list):
+    start = list[0]
+    end = list[1]
 
-        for map in maps[1]:
-            if soil >= map[1] and soil <= map[1] + map[2] - 1:
-                fert = map[0] + (soil - map[1])
-                found = True
-        if not found:
-            fert = soil
-        found = False
+    for mapIndex in range(0, 7):
+        for map in maps[mapIndex]:
+            if start >= map[1] and start <= map[1] + map[2] - 1:
+                if end <= map[1] + map[2] - 1:
+                    diff = map[1] - map[0]
+                    start -= diff
+                    end -= diff
+                else:
+                    diff = map[1] - map[0]
+                    mapTo([map[1] + map[2], end])
+                    start -= diff
+                    end = map[1] + map[2] - 1 - diff
+                break
+            elif end >= map[1] and end <= map[1] + map[2] - 1:
+                # start of input doesn't start in the middle of a map but the end does
+                diff = map[1] - map[0]
+                mapTo([start, map[1]-1])
+                start = map[1]-diff
+                end -= diff
 
-        for map in maps[2]:
-            if fert >= map[1] and fert <= map[1] + map[2] - 1:
-                water = map[0] + (fert - map[1])
-                found = True
-        if not found:
-            water = fert
-        found = False
+                break
+            elif start >= map[1] and end <=  map[1] + map[2] - 1:
+                # a range is fully encapsulated within the input
+                diff = map[1] - map[0]
+                mapTo([start, map[1]-1])
+                mapTo([map[1] + map[2], end])
+                start = map[1] - diff
+                end = map[1] + map[2] - 1 - diff
 
-        for map in maps[3]:
-            if water >= map[1] and water <= map[1] + map[2] - 1:
-                light = map[0] + (water - map[1])
-                found = True
-        if not found:
-            light = water
-        found = False
+                break
 
-        for map in maps[4]:
-            if light >= map[1] and light <= map[1] + map[2] - 1:
-                temp = map[0] + (light - map[1])
-                found = True
-        if not found:
-            temp = light
-        found = False
+    locationRanges.append([start, end])
 
-        for map in maps[5]:
-            if temp >= map[1] and temp <= map[1] + map[2] - 1:
-                humid = map[0] + (temp - map[1])
-                found = True
-        if not found:
-            humid = temp
-        found = False
-
-        for map in maps[6]:
-            if humid >= map[1] and humid <= map[1] + map[2] - 1:
-                loc = map[0] + (humid - map[1])
-                found = True
-        if not found:
-            loc = humid
-        found = False
-
-        if minLoc == 0:
-            minLoc = loc
-        else:
-            if loc < minLoc:
-                minLoc = loc
-        # print(minLoc)
-        # print(i)
-
-print(minLoc)
+for start in nStarts:
+    mapTo(start)
+        
+print(locationRanges)
 # 529571705 too high
